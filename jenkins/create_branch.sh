3f3bfe642edc82502f3b253d5a02ffa1273a4a41
Ondrej Janus <ojanus@redhat.com>
2020-01-21 13:36:48 +0100

    Add commit

--- jenkins/create_branch.sh
+++ jenkins/create_branch.sh
@@ -11,4 +11,8 @@ echo $(git status)
 
 CANDLEPIN_PREFIX='candlepin-'
 VERSION=$(echo "$CANDLEPIN_VERSION" | sed "s/$CANDLEPIN_PREFIX//")
-sed -i "s/${candlepin.version.placeholder}/$VERSION/g" pom.xml
+sed -i "s/\${candlepin.version.placeholder}/$VERSION/g" pom.xml
+
+git add pom.xml
+git commit -m "Automatic commit"
+git push origin $CANDLEPIN_VERSION
