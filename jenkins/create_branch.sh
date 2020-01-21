6aa66083644aa41cf31dd0c9ac9233da4882ea8d
Ondrej Janus <ojanus@redhat.com>
2020-01-21 16:07:23 +0100

    Refactor script

--- jenkins/create_branch.sh
+++ jenkins/create_branch.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
 
-CANDLEPIN_VERSION="candlepin-3.6.99"
-
-echo $(git status)
-git checkout master
-
-echo $(git status)
-git checkout -b $CANDLEPIN_VERSION
-echo $(git status)
+if [ "$1" != "" ]; then
+    VERSION="$1"
+else
+    echo "Candlepin version must be provided!"
+    exit 1
+fi
 
 CANDLEPIN_PREFIX='candlepin-'
+
+CANDLEPIN_VERSION=$(echo "$VERSION" | sed "s/$CANDLEPIN_PREFIX//")
+
+git checkout master
+git checkout -b $CANDLEPIN_VERSION
+
 VERSION=$(echo "$CANDLEPIN_VERSION" | sed "s/$CANDLEPIN_PREFIX//")
 sed -i "s/\${candlepin.version.placeholder}/$VERSION/g" pom.xml
 
 git add pom.xml
 git commit -m "Automatic commit"
-git push origin $CANDLEPIN_VERSION
+
+HOSTED_VERSION=$(grep -oPm1 "(?<=<version>)[^<]+" <<< $(head -n 20 pom.xml))
+TAG="${CANDLEPIN_VERSION}_${HOSTED_VERSION}"
+git tag $TAG
+
+BRANCH_NAME=$VERSION
+git push --follow-tags origin $BRANCH_NAME
