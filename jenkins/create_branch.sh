2db3d7127a28edef86fd0bbb1fc858f7733e1e5a
Ondrej Janus <ojanus@redhat.com>
2020-01-21 13:08:13 +0100

    Remove jenkins arg

--- jenkins/create_branch.sh
+++ jenkins/create_branch.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
+CANDLEPIN_VERSION="candlepin-3.6.99"
+
 echo $(git status)
 git checkout master
 
 echo $(git status)
-git checkout -b ${params.version}
+git checkout -b $CANDLEPIN_VERSION
 echo $(git status)
 
-VERSION="candlepin-3.6.99"
 CANDLEPIN_PREFIX = 'candlepin-'
-CANDLEPIN_VERSION=$(echo "$VERSION" | sed "s/$CANDLEPIN_PREFIX//")
-sed -i "s/${candlepin.version.placeholder}/$CANDLEPIN_VERSION/g" pom.xml
+VERSION=$(echo "$CANDLEPIN_VERSION" | sed "s/$CANDLEPIN_PREFIX//")
+sed -i "s/${candlepin.version.placeholder}/$VERSION/g" pom.xml
