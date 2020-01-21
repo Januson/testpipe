4b138f77168b3e22d37706bd3b67e289ff6b8b9d
Ondrej Janus <ojanus@redhat.com>
2020-01-21 16:22:18 +0100

    Cleanup

--- jenkins/create_branch.sh
+++ jenkins/create_branch.sh
@@ -8,19 +8,23 @@ else
 fi
 
 CANDLEPIN_PREFIX='candlepin-'
-
 BRANCH_NAME=$VERSION
+
 CANDLEPIN_VERSION=$(echo "$VERSION" | sed "s/$CANDLEPIN_PREFIX//")
 
+# Create a new branch based on master
 git checkout master
 git checkout -b $BRANCH_NAME
 
+# Update Candlepin version in pom.xml
 VERSION=$(echo "$CANDLEPIN_VERSION" | sed "s/$CANDLEPIN_PREFIX//")
 sed -i "s/\${candlepin.version.placeholder}/$VERSION/g" pom.xml
 
+# Commit changes
 git add pom.xml
-git commit -m "Automatic commit"
+git commit -m "Automatic commit - Target Candlepin build: '${VERSION}'"
 
+# Create tag
 HOSTED_VERSION=$(grep -oPm1 "(?<=<version>)[^<]+" <<< $(head -n 20 pom.xml))
 TAG="${CANDLEPIN_VERSION}_${HOSTED_VERSION}"
 git tag $TAG
